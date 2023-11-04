4c461dd09a57e9a8e2387f336ea9b81fbbe3c3a0
Shawn Aviles <shawnaviles26@gmail.com>
2023-11-04 18:08:35 -0400

    added translation functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,12 @@
 function Navbar() {
   return (
-    <div className='bg-cyan-300 w-screen h-32 flex flex-col items-between justify-center '>
+    <div className='bg-cyan-300 w-screen h-32 flex flex-row items-center justify-between '>
 		<div className="pl-10">
-			<h1 className="text-3xl">Jib Jargon</h1>
-			<p className="text-sm">Shawn, Christian, Rumi</p>
+			<h1 className="text-3xl font-bold">Jib Jargon</h1>
+			<p className="text-sm font-normal">Shawn, Christian, Rumi</p>
 		</div> 
-		<div className="w-">
-
+		<div className="pr-10">
+      <img src="https://picsum.photos/200/100" alt="" />
 		</div>
     </div>
   )
--- src/components/TermCard.jsx
+++ src/components/TermCard.jsx
@@ -1,19 +1,29 @@
 import PropTypes from 'prop-types';
 
-function TermCard({ title }) {
+function TermCard({ title, options, term = false, updateDefinition, definition }) {
 	return (
 		<div className="w-48 bg-cyan-100 flex flex-col justify-center items-center p-3 m-3 rounded-xl border-2">
 			<h4 className="text-xl w-full py-1">{title}</h4>
-			<select name="NauticalTerms" className="text-md bg-white w-full p-2">
-				<option value="bow">Bow</option>
-				<option value="stern">Stern</option>
-			</select>			
+			{(term && options) ? 
+				<select name="NauticalTerms" className="text-md bg-white w-full p-2 rounded-sm" onChange={updateDefinition}>
+				{options.map((key) => 
+					<option key={key.toLowerCase()} value={key}>{key}</option>
+				)}
+				</select>
+				:
+				<p className="text-md bg-white w-full p-2 rounded-sm">{definition}</p>	
+			}
+						
 		</div>
 	)
 }
 
 TermCard.propTypes = {
 	title: PropTypes.string.isRequired,
+	options: PropTypes.array,
+	term: PropTypes.bool,
+	definition: PropTypes.string,
+	updateDefinition: PropTypes.func,
 }
 
 export default TermCard;
