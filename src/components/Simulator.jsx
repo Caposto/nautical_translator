78f900b2a28ef335af41628ff1aec02f6aab3e14
Shawn Aviles <shawnaviles26@gmail.com>
2023-11-04 16:22:31 -0400

    rename and refactor translation section

--- src/components/Simulator.jsx
+++ src/components/Simulator.jsx
@@ -5,7 +5,7 @@ import Ocean from "./Ocean";
 
 function Simulator() {
   return (
-	<div className="w-full h-[500px] flex items-center justify-center">
+	<div className="w-full h-[450px] flex items-center justify-center">
 		<div className='bg-indigo-100 relative w-3/4 h-96'> 
 			<div className='h-full'>
 				<Canvas camera = {{
