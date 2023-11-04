03c7082cc71288a9911ba66ca54a6a85ff8bfccf
Shawn Aviles <shawnaviles26@gmail.com>
2023-11-04 15:47:09 -0400

    refactor and base layout

---
+++ src/components/TranslateWrapper.jsx
@@ -0,0 +1,11 @@
+import TermCard from "./TermCard";
+
+function TranslateWrapper() {
+	return (
+		<div className="w-full flex justify-center items-center p-4">
+			<TermCard />
+		</div>
+	)
+}
+
+export default TranslateWrapper;
