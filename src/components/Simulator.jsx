86bbf1e2ab6bf3ef5f71a3e87301538085893f85
Christian Apostol <christiand.apostol@outlook.com>
2023-11-04 16:16:27 -0400

    peak camera angle

--- src/components/Simulator.jsx
+++ src/components/Simulator.jsx
@@ -9,14 +9,14 @@ function Simulator() {
 		<div className='bg-indigo-100 relative w-3/4 h-96'> 
 			<div className='h-full'>
 				<Canvas camera = {{
-					position: [0,5,18],
+					position: [0,10,18],
 					fov:45,
 					}}>
 					<color attach="background" args={["#ececec"]}/>
 					<OrbitControls/> 
 					<ambientLight intensity={1} />
 					<directionalLight position={[0, 0, 5]} intensity={2} />
-					<Boat scale={0.01} scenePosition={[0, 0.5, 0]}/>
+					<Boat scale={0.01} scenePosition={[0, 0.5, 3]}/>
 					<Ocean />
 					<gridHelper />
 					<axesHelper args={[5]}/>
