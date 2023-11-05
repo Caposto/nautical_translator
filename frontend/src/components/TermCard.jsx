31c61fdfcf5884d77cc52994cd5685ea1afb337f
j-ferber <ferberjustin5@gmail.com>
2023-11-05 02:25:39 -0500

    Added the use of the database for the dropdown containing the parts of the boat

--- frontend/src/components/TermCard.jsx
+++ frontend/src/components/TermCard.jsx
@@ -5,9 +5,9 @@ function TermCard({ title, options, updateDefinition }) {
 		<div className="w-48 bg-cyan-100 flex flex-col justify-center items-center p-3 m-3 rounded-xl border-2">
 			<h4 className="text-xl w-full py-1">{title}</h4>
 			<select name="NauticalTerms" className="text-md bg-white w-full p-2 rounded-sm" onChange={updateDefinition}>
-			{options.map((key) => 
-				<option key={key.toLowerCase()} value={key}>{key}</option>
-			)}
+			{options.map((option) => 
+				<option key={option.id} value={option.name}>{option.name}</option>
+      )}
 			</select>
 		</div>
 	)
--- frontend/src/components/TranslationCard.jsx
+++ frontend/src/components/TranslationCard.jsx
@@ -4,14 +4,14 @@ function TermCard({ title, definition }) {
 	return (
 		<div className="w-64 bg-cyan-100 flex flex-col justify-center items-center p-3 m-3 rounded-xl border-2">
 			<h4 className="text-xl w-full py-1">{title}</h4>
-			<p className="text-md bg-white w-full p-2 rounded-sm">{definition}</p>	
+			<p className="text-md bg-white w-full p-2 rounded-sm">{definition.definition}</p>	
 		</div>
 	)
 }
 
 TermCard.propTypes = {
 	title: PropTypes.string.isRequired,
-	definition: PropTypes.string,
+	definition: PropTypes.object,
 }
 
 export default TermCard;
