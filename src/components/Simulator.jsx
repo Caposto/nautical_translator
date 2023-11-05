f31443d3ee00656293673fb324f23a907cbbfe65
Shawn <43704084+ShawnAviles@users.noreply.github.com>
2023-11-04 21:44:58 -0400

    Frontend (#6)
    * small changes
    
    * updated navbar and helper icon

--- src/components/Simulator.jsx
+++ src/components/Simulator.jsx
@@ -6,22 +6,22 @@ import Ocean from './Ocean';
 function Simulator() {
 
   return (
-    <div className="w-full h-[450px] flex items-center justify-center">
-      <div className='bg-indigo-100 relative w-3/4 h-96'> 
-        <div className='h-full'>
-          <Canvas camera={{
-            position: [0, 10, 18],
-            fov: 45,
-          }}>
-            <color attach="background" args={["#ececec"]}/>
-            <OrbitControls minDistance={5} maxDistance={20} maxPolarAngle={Math.PI / 2 - 0.1} /> 
-            <ambientLight intensity={1} />
-            <directionalLight position={[0, 0, 5]} intensity={2} />
-            <Boat scale={0.01} scenePosition={[0, 0.75, 3]}/>
-            <Ocean />
-          </Canvas>
-        </div>
-      </div>
+	<div className="w-full h-[525px] flex items-center justify-center">
+		<div className='relative w-5/6 h-[500px] drop-shadow-2xl'> 
+			<div className='h-full'>
+			<Canvas camera={{
+				position: [0, 10, 18],
+				fov: 45,
+			}}>
+				<color attach="background" args={["#ececec"]}/>
+				<OrbitControls minDistance={5} maxDistance={20} maxPolarAngle={Math.PI / 2 - 0.1} /> 
+				<ambientLight intensity={1} />
+				<directionalLight position={[0, 0, 5]} intensity={2} />
+				<Boat scale={0.01} scenePosition={[0, 0.75, 3]}/>
+				<Ocean />
+			</Canvas>
+			</div>
+		</div>
     </div>
   )
 }
--- src/components/Translater.jsx
+++ src/components/Translater.jsx
@@ -1,9 +1,10 @@
 import TermCard from "./TermCard";
+import TranslationCard from "./TranslationCard";
 import Arrow from "../assets/Arrow.svg";
 import { useState } from "react";
 
 function Translater() {
-	const defaultSelectOptions = {
+	let defaultSelectOptions = {
 		Bow: "The front part of a boat.",
 		Stern: "The rear part of a boat.",
 		Port: "The left side of a boat when facing forward.",
@@ -19,9 +20,9 @@ function Translater() {
 
 	return (
 		<div className="w-full flex justify-center items-center p-4">
-			<TermCard title="Nautical Term" updateDefinition={handleChange} options={Object.keys(defaultSelectOptions)} term/>
+			<TermCard title="Nautical Term" updateDefinition={handleChange} options={Object.keys(defaultSelectOptions)} />
 			<img src={Arrow} alt="Arrow" />
-			<TermCard title="Translation" definition={defaultSelectOptions[currentSelection]} />
+			<TranslationCard title="Translation" definition={defaultSelectOptions[currentSelection]} />
 		</div>
 	)
 }
