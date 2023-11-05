6e2d722ced340c34456f3de6b0f2bbcc30977f49
Christian Apostol <christiand.apostol@outlook.com>
2023-11-05 02:43:56 -0500

    got rid of context

--- frontend/src/utils/BoatContext.js
+++
@@ -1,4 +0,0 @@
-import { createContext } from "react";
-
-// Contains context for each boat parts scene position (camera) and mesh material (used to highlight selected parts)
-export const BoatContext = createContext({});
