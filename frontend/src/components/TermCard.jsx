ef936b5844609db12f2ae10c402ee30e902d0e8b
Justin Ferber <119906373+j-ferber@users.noreply.github.com>
2023-11-05 09:33:58 -0500

    Added material  ui boxes (#24)

--- frontend/src/components/TermCard.jsx
+++ frontend/src/components/TermCard.jsx
@@ -1,15 +1,25 @@
 import PropTypes from 'prop-types';
+import { Card, CardBody, Option, Select } from '@material-tailwind/react';
 
 function TermCard({ title, options, updateDefinition }) {
 	return (
-		<div className="w-48 bg-cyan-100 flex flex-col justify-center items-center p-3 m-3 rounded-xl border-2">
-			<h4 className="text-xl w-full py-1">{title}</h4>
-			<select name="NauticalTerms" className="text-md bg-white w-full p-2 rounded-sm" onChange={updateDefinition}>
-			{options.map((option) => 
-				<option key={option.id} value={option.name}>{option.name}</option>
-      )}
-			</select>
-		</div>
+		// <div className="w-48 bg-cyan-100 flex flex-col justify-center items-center p-3 m-3 rounded-xl border-2">
+		// 	<h4 className="text-xl w-full py-1">{title}</h4>
+		// 	<select name="NauticalTerms" className="text-md bg-white w-full p-2 rounded-sm" onChange={updateDefinition}>
+		// 	{options.map((option) => 
+		// 		<option key={option.id} value={option.name}>{option.name}</option>
+    //   )}
+		// 	</select>
+		// </div>
+    <Card className='mr-3 border-blue-600 border-2'>
+      <CardBody>
+        <Select label='Nautical Term:' onChange={updateDefinition}>
+          {options.map((option) => 
+            <Option key={option.id} value={option.name}>{option.name}</Option>
+          )}
+        </Select>
+      </CardBody>
+    </Card>
 	)
 }
 
