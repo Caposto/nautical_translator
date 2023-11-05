a55306fbe0bfbe2f8e466bd3f57418b367d156b9
Christian Apostol <christiand.apostol@outlook.com>
2023-11-05 03:29:42 -0500

    adjusted transparency

--- frontend/src/components/TriCircle.jsx
+++ frontend/src/components/TriCircle.jsx
@@ -4,7 +4,7 @@ export default function TriCircle({sceneRotation, color, id}) {
   return (
     <mesh rotation={sceneRotation} userData={{id}}>
       <circleGeometry args={[20, 32, 0, Math.PI * 2/3]} />
-      <meshBasicMaterial color={color} side={THREE.DoubleSide} />
+      <meshBasicMaterial color={color} side={THREE.DoubleSide} transparent={true} opacity={0.5}/>
     </mesh>
   );
 }
