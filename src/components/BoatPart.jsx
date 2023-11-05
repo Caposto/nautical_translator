9b2ac3526f7560ceee50ead1c61a24e2f4b4605a
Christian Apostol <christiand.apostol@outlook.com>
2023-11-04 21:33:23 -0400

    changed color of mesh

--- src/components/BoatPart.jsx
+++ src/components/BoatPart.jsx
@@ -1,15 +1,26 @@
-import { useLoader } from "@react-three/fiber";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { useLoader } from '@react-three/fiber';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { MeshStandardMaterial } from 'three';
 
 // eslint-disable-next-line react/prop-types
-export default function BoatPart({url, scale, name, id}) {
-    const part = useLoader(GLTFLoader, url);
+export default function BoatPart({ url, scale, name, id, color }) {
+  const part = useLoader(GLTFLoader, url);
 
-    return (
-      <primitive 
-        object={part.scene}
-        scale={scale} 
-        userData={{name: name, id: id}}
-      />
-    );
+  // Create a custom material
+  const customMaterial = new MeshStandardMaterial({ color: color || 'white' });
+
+  // Traverse the loaded GLTF and change the material
+  part.scene.traverse((child) => {
+    if (child.isMesh) {
+      child.material = customMaterial;
+    }
+  });
+
+  return (
+    <primitive 
+      object={part.scene}
+      scale={scale}
+      userData={{ name: name, id: id }}
+    />
+  );
 }
