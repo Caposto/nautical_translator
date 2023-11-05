832f7175205c464a8d5d94da707df7667c64091a
j-ferber <ferberjustin5@gmail.com>
2023-11-05 01:14:52 -0500

    added cors for localhost

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,10 @@ require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const requestRoutes = require('./routes/requestRoutes')
+const corsOptions = require('./config/corsOptions')
 const app = express()
 
+app.use(cors(corsOptions))
 app.use(express.json())
 app.use('/request', requestRoutes)
 
