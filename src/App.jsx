470d066d2ab698748e3f6f62d08e04414ccaa3b8
Christian Apostol <94158310+Caposto@users.noreply.github.com>
2023-11-04 23:08:48 -0400

    Feature/boat view (#7)
    * added custom boat group
    
    * removed test group
    
    * refactor and base layout
    
    * rename and refactor translation section
    
    * Frontend (#6)
    
    * small changes
    
    * updated navbar and helper icon
    
    * adjusted max position
    
    * updated boat rotation to point forward
    
    * changed color of mesh
    
    ---------
    
    Co-authored-by: Shawn Aviles <shawnaviles26@gmail.com>
    Co-authored-by: Shawn <43704084+ShawnAviles@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import Simulator from './components/Simulator';
 import Navbar from './components/Navbar';
 import Translater from './components/Translater';
+import { BoatContext } from './utils/BoatContext';
+import Boat from './components/Boat';
 
 function App() {
+  // Boat Context Wraps the Simulation & Translater (user input)
+  // Changes in the Translater update the Boat Context
+    // i.e user selects "hull", the camera should lookAt the hull and change hull's mesh material to highlight it
+    // Only select user inputs are acted on in the Boat Context - see Boat.jsx for the 6 parts
   return (
     <div className='bg-blue-1 w-full h-full overflow-hidden'> 
       <Navbar />
-      <Simulator />
-      <Translater />
+      <BoatContext.Provider value={Boat}>
+        <Simulator />
+        <Translater />
+      </BoatContext.Provider>
     </div>
   )
 }
