cd78d718dd9ba8de114baccc3b7af77cb282900f
Christian Apostol <christiand.apostol@outlook.com>
2023-11-05 00:07:26 -0400

    successfully highlighting things

--- src/components/BoatPart.jsx
+++ src/components/BoatPart.jsx
@@ -1,33 +1,33 @@
-import { useState } from 'react';
+import { useEffect, useRef } from 'react';
 import { useLoader } from '@react-three/fiber';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { MeshStandardMaterial } from 'three';
 
 // eslint-disable-next-line react/prop-types
-export default function BoatPart({ url, scale, name, id, color, highlightColor }) {
-  const [active, setActive] = useState(false);
+export default function BoatPart({ url, scale, name, id, color, highlightColor, activeParts }) {
   const part = useLoader(GLTFLoader, url);
 
   // Create a custom material
-  const customMaterial = new MeshStandardMaterial({ color: color || 'white' });
+  const customMaterialRef = useRef(new MeshStandardMaterial({ color: color || 'white' }));
+  const highlightMaterialRef = useRef(new MeshStandardMaterial({ color: highlightColor || 'yellow' })); // Default highlight color set to yellow
 
-  // Attach a useEffect that runs when active changes
-  // Change active from the buttons in Simulator.jsx
-
-  // Set intial color
-  part.scene.traverse((child) => {
-    if (child.isMesh) {
-      child.material = customMaterial;
-    }
-  });
-
-  // If active, change the color (different color for each component)
+  // Check if the part is active and should be highlighted
+  useEffect(() => {
+    // eslint-disable-next-line react/prop-types
+    const isActive = activeParts.has(id); // Check if the part's name is in the activeParts set
+    part.scene.traverse((child) => {
+      if (child.isMesh) {
+        // Apply the highlight material if active, otherwise the regular material
+        child.material = isActive ? highlightMaterialRef.current : customMaterialRef.current;
+      }
+    });
+  }, [activeParts, name, part.scene]); // Depend on activeParts, name, and part.scene to rerun this effect
 
   return (
     <primitive 
       object={part.scene}
       scale={scale}
-      userData={{ name: name, id: id }}
+      userData={{ name, id }}
     />
   );
 }
--- src/components/Simulator.jsx
+++ src/components/Simulator.jsx
@@ -1,20 +1,33 @@
 import { Canvas } from '@react-three/fiber';
+import { useState } from 'react';
 import { OrbitControls } from '@react-three/drei';
 import Boat from './Boat';
 import Ocean from './Ocean';
 import { Button } from "@material-tailwind/react";
 
 function Simulator() {
+	const [activeParts, setActiveParts] = useState(new Set()); // Hashmap of part names
+
+	// Since sets are mutable have to add additional add/remove functions to update state
+	const editPart = part => {
+		// Remove Part if it already exists
+		if (activeParts.has(part)) {
+			setActiveParts(prev => new Set([...prev].filter(x => x !== part)))
+		}
+		else {
+			setActiveParts(previousState => new Set([...previousState, part]))
+		}
+	}
 
   return (
 	<div className="w-full h-[500px] my-4 flex items-center justify-center">
 		<div className="flex flex-col h-full justify-around">
-			<Button className='bg-blue-600 text-white flex-grow m-3 text-lg'>Words</Button>
-			<Button className='bg-blue-600 text-white flex-grow m-3 text-lg'>Words</Button>
-			<Button className='bg-blue-600 text-white flex-grow m-3 text-lg'>Words</Button>
-			<Button className='bg-blue-600 text-white flex-grow m-3 text-lg'>Words</Button>
-			<Button className='bg-blue-600 text-white flex-grow m-3 text-lg'>Words</Button>
-			<Button className='bg-blue-600 text-white flex-grow m-3 text-lg'>Words</Button>
+			<Button className='bg-blue-600 text-white flex-grow m-3 text-lg' onClick={() => editPart("hull")}>Hull</Button>
+			<Button className='bg-blue-600 text-white flex-grow m-3 text-lg' onClick={() => editPart("keel")}>Keel</Button>
+			<Button className='bg-blue-600 text-white flex-grow m-3 text-lg' onClick={() => editPart("rudder")}>Rudder</Button>
+			<Button className='bg-blue-600 text-white flex-grow m-3 text-lg' onClick={() => editPart("mast")}>Mast</Button>
+			<Button className='bg-blue-600 text-white flex-grow m-3 text-lg' onClick={() => editPart("sail")}>Sail</Button>
+			<Button className='bg-blue-600 text-white flex-grow m-3 text-lg' onClick={() => editPart("boom")}>Boom</Button>
 		</div>
 		<div className='relative w-[675px] h-[500px] drop-shadow-2xl'> 
 			<div className='h-full'>
@@ -26,7 +39,7 @@ function Simulator() {
 				<OrbitControls minDistance={5} maxDistance={30} maxPolarAngle={Math.PI / 2 - 0.1} /> 
 				<ambientLight intensity={1} />
 				<directionalLight position={[0, 0, 5]} intensity={2} />
-				<Boat scale={0.01} scenePosition={[0, 0.5, 3]}/>
+				<Boat scale={0.01} scenePosition={[0, 0.5, 3]} activeParts={activeParts}/>
 				<Ocean />
 			</Canvas>
 			</div>
