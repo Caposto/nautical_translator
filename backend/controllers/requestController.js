34196d98882e60e2e4e2e6d1045b6eb3b21f0024
Justin Ferber <119906373+j-ferber@users.noreply.github.com>
2023-11-05 01:05:17 -0400

    added backend server (#10)
    * added backend server
    
    * env example
    
    * final refactor for backend
    
    ---------
    
    Co-authored-by: Shawn Aviles <shawnaviles26@gmail.com>

---
+++ backend/controllers/requestController.js
@@ -0,0 +1,21 @@
+const getAll = async (req, res) => {
+  const data = []
+  for (i = 1; i < 16; i++) {
+     const response = await fetch(`https://rumilog.kintone.com/k/v1/record.json?app=2&id=${i}`, {
+      method: 'GET', // The method is GET
+      headers: {
+        'X-Cybozu-API-Token': process.env.KINTONE_TOKEN // Setting the header with your API token
+      }
+    })
+    const jsonResponse = await response.json()
+    data.push({
+      id: jsonResponse.record.Record_number.value,
+      name: jsonResponse.record.Drop_down.value,
+      definition: jsonResponse.record.Text.value
+    })
+    console.log(jsonResponse)
+  }
+  res.json(data)
+}
+
+module.exports = {getAll}
