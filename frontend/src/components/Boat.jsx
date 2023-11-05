8dc20b7cd4cd7bb2ebf00e1f97b4d9c8325d2f97
Justin Ferber <119906373+j-ferber@users.noreply.github.com>
2023-11-05 04:51:30 -0500

    Added color and state (#21)

--- frontend/src/components/Boat.jsx
+++ frontend/src/components/Boat.jsx
@@ -1,7 +1,7 @@
 import { useRef, useEffect } from "react";
 import BoatPart from "./BoatPart";
 
-export default function Boat({scale, scenePosition, activeParts}) {
+export default function Boat({scale, scenePosition, activePart}) {
   const groupRef = useRef();
   const rotationInRadians = 90 * (Math.PI / 180);
 
@@ -34,12 +34,12 @@ export default function Boat({scale, scenePosition, activeParts}) {
 
   return (
     <group ref={groupRef} position={scenePosition} rotation={[rotationInRadians, 0, rotationInRadians * 2]}>
-      <BoatPart url='src/assets/Hull.glb' scale={scale} name="Hull" id="hull" color="#406882" highlightColor="#FF0000" activeParts={activeParts}/>
-      <BoatPart url='src/assets/Keel.glb' scale={scale} name="Keel" id="keel" color="#8B4513" highlightColor="#FF0000" activeParts={activeParts}/>
-      <BoatPart url='src/assets/Rudder.glb' scale={scale} name="Rudder" id="rudder" color="#5C4033" highlightColor="#FF0000" activeParts={activeParts}/>
-      <BoatPart url='src/assets/Mast.glb' scale={scale} name="Mast" id="mast" color="#D2B48C" highlightColor="#FF0000" activeParts={activeParts}/>
-      <BoatPart url='src/assets/Sail.glb' scale={scale} name="Sail" id="sail" color="#F8F8FF" highlightColor="#FF0000" activeParts={activeParts}/>
-      <BoatPart url='src/assets/Boom.glb' scale={scale} name="Boom" id="boom" color="#3B444B" highlightColor="#FF0000" activeParts={activeParts}/>
+      <BoatPart url='src/assets/Hull.glb' scale={scale} name="Hull" id="hull" color="#406882" highlightColor="#FF0000" activePart={activePart}/>
+      <BoatPart url='src/assets/Keel.glb' scale={scale} name="Keel" id="keel" color="#8B4513" highlightColor="#FF0000" activePart={activePart}/>
+      <BoatPart url='src/assets/Rudder.glb' scale={scale} name="Rudder" id="rudder" color="#5C4033" highlightColor="#FF0000" activePart={activePart}/>
+      <BoatPart url='src/assets/Mast.glb' scale={scale} name="Mast" id="mast" color="#D2B48C" highlightColor="#FF0000" activePart={activePart}/>
+      <BoatPart url='src/assets/Sail.glb' scale={scale} name="Sail" id="sail" color="#F8F8FF" highlightColor="#FF0000" activePart={activePart}/>
+      <BoatPart url='src/assets/Boom.glb' scale={scale} name="Boom" id="boom" color="#3B444B" highlightColor="#FF0000" activePart={activePart}/>
     </group>
   );
 }
