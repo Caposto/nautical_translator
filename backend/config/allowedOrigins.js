e2bda83af63823094c462da80fd3dc2d614c374d
j-ferber <ferberjustin5@gmail.com>
2023-11-05 10:09:38 -0500

    added link

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://nautical-translator-frontend.onrender.com/"
+  "https://nautical-translator-frontend.onrender.com/",
+  'https://nautical-translator.vercel.app/'
 ]
 
 module.exports = allowedOrigins
