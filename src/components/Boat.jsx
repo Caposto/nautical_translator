e7ac98f98c87907cbdf11b19c894f61784355103
Christian Apostol <christiand.apostol@outlook.com>
2023-11-04 23:19:38 -0400

    added base colors for boat

--- src/components/Boat.jsx
+++ src/components/Boat.jsx
@@ -9,12 +9,12 @@ export default function Boat({scale, scenePosition}) {
 
   return (
     <group ref={groupRef} position={scenePosition} rotation={[rotationInRadians, 0, rotationInRadians * 2]}>
-      <BoatPart url='src/assets/Hull.glb' scale={scale} name="Hull" id="hull" color="#ff0000"/>
-      <BoatPart url='src/assets/Keel.glb' scale={scale} name="Keel" id="keel"/>
-      <BoatPart url='src/assets/Rudder.glb' scale={scale} name="Rudder" id="rudder"/>
-      <BoatPart url='src/assets/Mast.glb' scale={scale} name="Mast" id="mast"/>
-      <BoatPart url='src/assets/Sail.glb' scale={scale} name="Sail" id="sail"/>
-      <BoatPart url='src/assets/Boom.glb' scale={scale} name="Boom" id="boom"/>
+      <BoatPart url='src/assets/Hull.glb' scale={scale} name="Hull" id="hull" color="#406882"/>
+      <BoatPart url='src/assets/Keel.glb' scale={scale} name="Keel" id="keel" color="#8B4513"/>
+      <BoatPart url='src/assets/Rudder.glb' scale={scale} name="Rudder" id="rudder" color="#5C4033"/>
+      <BoatPart url='src/assets/Mast.glb' scale={scale} name="Mast" id="mast" color="#D2B48C"/>
+      <BoatPart url='src/assets/Sail.glb' scale={scale} name="Sail" id="sail" color="#F8F8FF"/>
+      <BoatPart url='src/assets/Boom.glb' scale={scale} name="Boom" id="boom" color="#3B444B"/>
     </group>
   );
 }
--- src/components/BoatPart.jsx
+++ src/components/BoatPart.jsx
@@ -1,20 +1,27 @@
+import { useState } from 'react';
 import { useLoader } from '@react-three/fiber';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { MeshStandardMaterial } from 'three';
 
 // eslint-disable-next-line react/prop-types
-export default function BoatPart({ url, scale, name, id, color }) {
+export default function BoatPart({ url, scale, name, id, color, highlightColor }) {
+  const [active, setActive] = useState(false);
   const part = useLoader(GLTFLoader, url);
 
   // Create a custom material
   const customMaterial = new MeshStandardMaterial({ color: color || 'white' });
 
-  // Traverse the loaded GLTF and change the material
+  // Attach a useEffect that runs when active changes
+  // Change active from the buttons in Simulator.jsx
+
+  // Set intial color
   part.scene.traverse((child) => {
     if (child.isMesh) {
       child.material = customMaterial;
     }
   });
+
+  // If active, change the color (different color for each component)
 
   return (
     <primitive 
