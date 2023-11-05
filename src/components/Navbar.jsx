84354ae923b6dd9d123717581998b5a5815a9dec
Shawn <43704084+ShawnAviles@users.noreply.github.com>
2023-11-04 23:40:08 -0400

    Frontend (#8)
    * small changes
    
    * updated navbar and helper icon
    
    * buttons and font

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,37 @@
+import { useState } from 'react';
 import scuba_fish_swimming4 from '../assets/scuba_fish_swimming4.gif';
 
 function Navbar() {
+	const [showInstructions, setShowInstructions] = useState(false);
+
   return (
     <div className='bg-gradient-to-b from-sky-600 to-white w-screen h-24 flex flex-row items-center justify-between '>
 		<div className="pl-10">
 			<h1 className="text-3xl font-extrabold">Jib Jargon</h1>
 			<p className="text-sm font-normal">Shawn, Christian, Rumi</p>
 		</div> 
-		<div className="pr-10">
-            <img src={scuba_fish_swimming4} alt="" width={80} height={80} className='bg-cyan-100 rounded-full border-md'/>
-		</div>
+		<div className="pr-10 flex flex-row"> 
+			{showInstructions && 
+				<div alt="message-bubble" className='flex items-center'>
+					<div class="flex items-center justify-end">
+						<div class="bg-blue-200 p-2 rounded-lg text-sm w-64">
+							Learn all about Nautical Terms via the defintions and visualzation below!
+						</div>
+						<div class="w-3 overflow-hidden ">
+							<div class="h-4 bg-blue-200 rotate-45 transform origin-top-left rounded-sm"></div>
+						</div>
+					</div>
+				</div>
+			}
+			<img 
+				src={scuba_fish_swimming4} 
+				className='w-[80px] h-[80px] bg-cyan-100 rounded-full border cursor-pointer hover:scale-x transform transition duration-y'
+				alt="" 
+				width={80} 
+				height={80}
+				onClick={() => setShowInstructions(!showInstructions)} 
+			/>
+			</div>
     </div>
   )
 }
