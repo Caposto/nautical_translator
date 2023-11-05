698a2815bb59c5abc74b0dc82f4d33ea253b7f62
Christian Apostol <94158310+Caposto@users.noreply.github.com>
2023-11-04 23:08:48 -0400

    Feature/boat view (#7)
    * added custom boat group
    
    * removed test group
    
    * refactor and base layout
    
    * rename and refactor translation section
    
    * Frontend (#6)
    
    * small changes
    
    * updated navbar and helper icon
    
    * adjusted max position
    
    * updated boat rotation to point forward
    
    * changed color of mesh
    
    ---------
    
    Co-authored-by: Shawn Aviles <shawnaviles26@gmail.com>
    Co-authored-by: Shawn <43704084+ShawnAviles@users.noreply.github.com>

---
+++ src/utils/BoatContext.js
@@ -0,0 +1,4 @@
+import { createContext } from "react";
+
+// Contains context for each boat parts scene position (camera) and mesh material (used to highlight selected parts)
+export const BoatContext = createContext({});
